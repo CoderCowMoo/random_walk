7a5a1c162648523141f742f087e7bfcca3cd3ee4
CoderCowMoo <broskichad38@gmail.com>
2024-03-25 22:52:30 +1100

    Changed to flo_draw library and triangle example

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,23 @@
-use draw::*;
+use flo_draw::*;
+use flo_canvas::*;
+
+pub fn main() {
+    with_2d_graphics(|| {
+        let canvas = create_canvas_window("Hello, triangle");
+
+        canvas.draw(|gc| {
+            gc.clear_canvas(Color::Rgba(0.0, 0.4, 0.4, 1.0));
+            gc.canvas_height(1000.0);
+            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
+
+            gc.new_path();
+            gc.move_to(200.0, 200.0);
+            gc.line_to(800.0, 200.0);
+            gc.line_to(500.0, 800.0);
+            gc.line_to(200.0, 200.0);
+
+            gc.fill_color(Color::Rgba(0.0, 0.0, 0.8, 1.0));
+            gc.fill();
+        });
+    });
+}
